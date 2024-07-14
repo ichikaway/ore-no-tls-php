99fcd088ccbecea46e7d30a0ad3f3bee5ebac7eb
ichikaway <ichikaway@gmail.com>
2024-07-14 17:33:20 +0900

    PRFでマスターシークレット作成まで実装

---
+++ src/Tls/Prf.php
@@ -0,0 +1,45 @@
+<?php
+namespace PHPTLS\Tls;
+
+final class Prf {
+
+    /**
+     * マスターシークレットを作成
+     *
+     * @param int $len
+     * @param string $secret bin
+     * @param string $clientRandom bin
+     * @param string $serverRandom bin
+     * @return string bin
+     * @throws \Exception
+     */
+    public static function createMasterSecret(int $len, string $secret, string $clientRandom, string $serverRandom): string
+    {
+        if (ctype_xdigit($clientRandom) || ctype_xdigit($serverRandom) || ctype_xdigit($secret)) {
+            throw new \Exception('Forbid hex data in createHash()');
+        }
+        $seed = 'master secret' . $clientRandom . $serverRandom;
+        return self::pHash($len, $secret, $seed);
+    }
+
+    /**
+     * HMAC作成
+     *
+     * @param int $len
+     * @param string $secret  bin
+     * @param string $seed bin
+     * @return string bin
+     * @throws \Exception
+     */
+    public static function pHash(int $len, string $secret, string $seed): string
+    {
+        if (ctype_xdigit($seed)  || ctype_xdigit($secret)) {
+            throw new \Exception('Forbid hex data in createHash()');
+        }
+        $hashed = hash_hkdf('sha256', $secret, $len, 'sha56', $seed);
+        if ($hashed === false) {
+            throw new \Exception('can not create hmac.');
+        }
+        return $hashed;
+    }
+}
---
+++ tests/PrfTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace PHPTLS\Tests;
+
+use PHPTLS\Tls\Prf;
+use PHPUnit\Framework\TestCase;
+
+class PrfTest extends TestCase
+{
+    public function testCreateHash()
+    {
+        $len = 48;
+        $secret = '0101010101';
+        $clientRandom = '0101010101010101010102020202020202020202030303030303030303030404';
+        $serverRandom = '0201010101010101010102020202020202020202030303030303030303030404';
+        $label = 'master secret';
+        $result = Prf::createMasterSecret($len, hex2bin($secret), $label, hex2bin($clientRandom), hex2bin($serverRandom));
+        $this->assertEquals($len, strlen($result));
+    }
+
+}
+
+?>
