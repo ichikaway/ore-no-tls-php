1e2a1ca65843c856cc87c3f73e720f8eda383683
ichikaway <ichikaway@gmail.com>
2024-07-15 22:01:17 +0900

    master secretからkey blockの配列を生成するメソッドとテストケースを作成

--- src/Tls/Prf.php
+++ src/Tls/Prf.php
@@ -23,6 +23,28 @@ final class Prf
         return self::pHash($len, $secret, $seed);
     }
 
+    /**
+     * @param string $secret bin
+     * @param string $clientRandom bin
+     * @param string $serverRandom bin
+     * @return array <string, string> bin
+     * @throws \Exception
+     */
+    public static function createKeyBlock(string $secret, string $clientRandom, string $serverRandom): array
+    {
+        if (ctype_xdigit($clientRandom) || ctype_xdigit($serverRandom) || ctype_xdigit($secret)) {
+            throw new \Exception('Forbid hex data in createHash()');
+        }
+        $seed = 'key expansion' . $serverRandom. $clientRandom; //key expansionはserver random, client randomの順に繋げる
+        $len = 40; //key blockは40byte固定
+        $hashed = self::pHash($len, $secret, $seed);
+        return [
+            "ClientWriteKey" => substr($hashed, 0, 16),
+            "ServerWriteKey" => substr($hashed, 16, 16),
+            "ClientWriteIV"  => substr($hashed, 32, 4),
+            "ServerWriteIV"  => substr($hashed, 36, 4),
+        ];
+    }
 
     /**
      * HMAC作成
--- tests/PrfTest.php
+++ tests/PrfTest.php
@@ -16,4 +16,16 @@ class PrfTest extends TestCase
         $result = Prf::createMasterSecret(hex2bin($secret), hex2bin($clientRandom), hex2bin($serverRandom));
         $this->assertEquals($len, strlen($result));
     }
+
+    public function testCreateKeyBlock()
+    {
+        $secret = '0101010101';
+        $clientRandom = '0101010101010101010102020202020202020202030303030303030303030404';
+        $serverRandom = '0201010101010101010102020202020202020202030303030303030303030404';
+        $result = Prf::createKeyBlock(hex2bin($secret), hex2bin($clientRandom), hex2bin($serverRandom));
+        $this->assertEquals(16, strlen($result['ClientWriteKey']));
+        $this->assertEquals(16, strlen($result['ServerWriteKey']));
+        $this->assertEquals(4, strlen($result['ClientWriteIV']));
+        $this->assertEquals(4, strlen($result['ServerWriteIV']));
+    }
 }
