44db29c21f74a140634637599c462438bfdae5bb
ichikaway <ichikaway@gmail.com>
2024-10-31 22:44:53 +0900

    自作TCPを使ったコネクターを準備

--- src/Tls/ConnectionOreNoTcp.php
+++ src/Tls/ConnectionOreNoTcp.php
@@ -13,40 +13,35 @@ class ConnectionOreNoTcp
     private string $ip;
     private int $port;
 
+    private string $srcIp;
+
+    private PhpTcp $PhpTcp;
+
     /**
      * @param $host
      */
-    public function __construct(string $host, ?int $port = null)
+    public function __construct(string $host, int $port = 443, string $srcIp = '')
     {
         $this->ip = gethostbyname($host);
-        $this->port = ($port === null) ? 443 : $port;
+        $this->port = $port;
         $this->host = $host;
+        $this->srcIp = $srcIp;
     }
 
     public function connect(): void
     {
+        $this->PhpTcp = new PhpTcp($this->srcIp);
+
         $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
         try {
-            if ($socket === false) {
-                //echo "Socket creation failed: " . socket_strerror(socket_last_error()) . "\n";
-                throw new \Exception("Socket creation failed: " . socket_strerror(socket_last_error()));
-            }
-
-            $result = socket_connect($socket, $this->ip, $this->port);
+            $result = $this->PhpTcp->connect($this->ip, $this->port);
             if ($result === false) {
                 //echo "Socket connect failed: " . socket_strerror(socket_last_error($socket)) . "\n";
                 throw new \Exception("Socket connect failed". socket_strerror(socket_last_error($socket)) . "\n");
             }
 
-            // ソケットオプションでタイムアウトを設定
-            socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, [
-                'sec' => 5, //5秒でタイムアウト
-                'usec' => 0,
-            ]);
-
-            $this->socket = $socket;
         } catch (\Exception $e) {
-            socket_close($socket);
+            $this->PhpTcp->close();
             throw $e;
         }
         echo "\nConnected to {$this->host}({$this->ip}):{$this->port}\n\n";
@@ -65,7 +60,7 @@ class ConnectionOreNoTcp
     {
         $dataAll = null;
         for ($i = 0; $i < 5; $i++) {
-            $dataAll .= $this->readData();
+            $dataAll .= $this->PhpTcp->read();
             if (TlsRecord::isEnoughData($dataAll)) {
                 break;
             } else {
@@ -77,24 +72,9 @@ class ConnectionOreNoTcp
     }
 
 
-    private function readData(): string
-    {
-        $recvAllData = null;
-        $size = 8000;
-        do {
-            if ($recvAllData !== null) {
-                // readのループが回っているか判断できるようにするecho
-                echo "  socket read again. \n";
-            }
-            $recv = socket_read($this->socket, $size);
-            $recvAllData .= $recv;
-        } while (strlen($recv) === $size);
-        return $recvAllData;
-    }
-
     public function write(string $data): int
     {
-        $result = socket_write($this->socket, $data, strlen($data));
+        $result = $this->PhpTcp->write($data);
         if ($result === false) {
             throw new \Exception('Socket write failed.' . socket_strerror(socket_last_error($this->socket)));
         }
@@ -103,6 +83,6 @@ class ConnectionOreNoTcp
 
     public function close(): void
     {
-        socket_close($this->socket);
+        $this->PhpTcp->close();
     }
 }
